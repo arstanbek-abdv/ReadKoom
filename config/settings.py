99130f7841a967cdca10f6a6d65f50eeeee56e5c
Arstanbek <abdyldaevarstanbek79@gmail.com>
2026-05-16 14:56:44 +0600

    Added R2 clouldflare configurations

--- config/settings.py
+++ config/settings.py
@@ -111,6 +111,18 @@ DATABASES = {
 
 AUTH_USER_MODEL = 'users.User'
 
+# Cloudflare R2 credentials
+AWS_ACCESS_KEY_ID = os.getenv("R2_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("R2_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = os.getenv("R2_BUCKET_NAME")
+AWS_S3_ENDPOINT_URL = f"https://{os.getenv('R2_ACCOUNT_ID')}.r2.cloudflarestorage.com"
+AWS_S3_REGION_NAME = "auto"
+
+AWS_QUERYSTRING_AUTH = False  # public URLs won't have auth params
+
+AWS_S3_FILE_OVERWRITE = False  # don't overwrite files with same name
+AWS_DEFAULT_ACL = None        # R2 doesn't support ACLs
+
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
