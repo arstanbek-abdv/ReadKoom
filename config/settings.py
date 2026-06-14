f80cd0193bfb533f948402b8b047bc29cadb2771
Arstanbek <abdyldaevarstanbek79@gmail.com>
2026-06-14 14:09:16 +0600

    Renamed DB variables in settings added .env* to gitignore

--- config/settings.py
+++ config/settings.py
@@ -101,9 +101,9 @@ SIMPLE_JWT = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME'),
+        'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432'
     }
